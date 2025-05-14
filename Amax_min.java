d3416af8c790e238aa43b57fee6da193cc72c050
Anjalirajak205 <anjalirajak205@gamil.com>
2025-05-14 13:30:11 +0530

    14 may

---
+++ Amax_min.java
@@ -0,0 +1,26 @@
+
+public class Amax_min {
+    public static void main(String[] args) {
+        int[] arr={60,1,89,5,3,6};
+        int max=0;
+        int min=arr[0];
+        
+
+        for(int i=0;i<arr.length;i++){
+           
+            if(arr[i]>max){
+            max=arr[i];
+            }
+
+            if(min>arr[i]){
+                min=arr[i];
+            }
+            
+        }
+        System.out.println("maximum element is "+max);
+        
+        System.out.println("minimum element is "+min);
+    }
+} 
+    
+
