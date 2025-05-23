80da64241d515559d1944aaf72bca91861a068b8
Anjalirajak205 <anjalirajak205@gamil.com>
2025-05-23 20:04:26 +0530

    23 may

---
+++ Cyclic_rotation.java
@@ -0,0 +1,22 @@
+public class Cyclic_rotation {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+       
+        for(int i=arr.length-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=arr.length;
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+            }
+        }
+    
+
+
+        }
+
+
+
+    
+
