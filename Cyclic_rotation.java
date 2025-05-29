f4e1f3b82ec736775540cfbbdc616284834b1121
Anjalirajak205 <anjalirajak205@gamil.com>
2025-05-29 21:55:10 +0530

    29 may

--- Cyclic_rotation.java
+++ Cyclic_rotation.java
@@ -1,23 +1,26 @@
 // questiion 7/50 
 public class Cyclic_rotation {
+   
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-       
-        for(int i=arr.length-1;i>0;i--){
-            arr[i]=arr[i-1];
-        }
-        arr[0]=arr.length;
 
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-            }
+        
+        int last = arr[arr.length - 1];
+
+        
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
         }
+
+        
+        arr[0] = last;
+
     
-
-
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-
-
+    }
+}
 
     
 
