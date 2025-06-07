808510b2df952fc39004939416c118ed45177b54
Anjalirajak205 <anjalirajak205@gmail.comgit config user.email>
2025-06-07 14:21:10 +0530

    07 june

--- maxSubarrayProduct.java
+++ maxSubarrayProduct.java
@@ -1,6 +1,31 @@
+// question 20/50...
+//07/06/2025
+
 public class maxSubarrayProduct {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        
     
+
+    public static int maxProduct(int[] arr) {
+        int maxProduct = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            int p = 1;
+            for (int j = i; j < arr.length; j++) {
+                p = p*arr[j];
+
+                // manually compare maxProduct and product
+                if (p > maxProduct) {
+                    maxProduct = p;
+                }
+            }
+        }
+
+        return maxProduct;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,2,-3,6};
+        System.out.println("Maximum product subarray: " + maxProduct(nums));
+    }
 }
+
+    
