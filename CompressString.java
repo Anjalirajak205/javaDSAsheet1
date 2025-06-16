e7db74f0aa0e6d60d8a795e53e30e560b8cbf967
Anjalirajak205 <anjalirajak205@gmail.com>
2025-06-16 20:52:41 +0530

    16 june

---
+++ CompressString.java
@@ -0,0 +1,24 @@
+public class CompressString {
+    public static void main(String[] args) {
+        String str ="hhheeelllooo ";
+        StringBuilder newStr = new StringBuilder();
+        int count =1;
+        //String str1="";
+       for(int i=1;i<str.length();i++){
+        if(str.charAt(i)==str.charAt(i-1)){
+            count++;
+        }
+        else{
+            newStr.append(str.charAt(i-1)).append(count);
+            count = 1;
+        }
+    }
+        System.out.println("Compressed string:"+newStr);
+
+    }
+}
+    
+
+
+    
+
