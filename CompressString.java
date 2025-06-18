2b6a272399cf5151618766ee42b3a228bd6ef91d
Anjalirajak205 <anjalirajak205@gmail.com>
2025-06-18 10:39:18 +0530

    18 june

--- CompressString.java
+++ CompressString.java
@@ -1,27 +1,38 @@
 // quest 31/50..
-// 16 june 
+//  16 june 
 
 public class CompressString {
-    public static void main(String[] args) {
-        String str ="hhheeelllooo ";
-        StringBuilder newStr = new StringBuilder();
-        int count =1;
-        //String str1="";
-       for(int i=1;i<str.length();i++){
-        if(str.charAt(i)==str.charAt(i-1)){
-            count++;
-        }
-        else{
-            newStr.append(str.charAt(i-1)).append(count);
-            count = 1;
-        }
-    }
-        System.out.println("Compressed string:"+newStr);
+    public static int compress(char[] chars) {
+        int index = 0; // Write pointer
+        int i = 0;     // Read pointer
 
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+
+            chars[index++] = currentChar;
+
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index; 
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'h', 'h', 'h', 'e', 'e', 'e', 'l', 'l', 'l', 'o', 'o', 'o'};
+        int newLength = compress(chars);
+
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(chars[i]);
+        }
     }
 }
-    
-
-
-    
-
