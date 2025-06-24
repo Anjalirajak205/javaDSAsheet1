1389f2d4a1c1e08e52385dbea2b9e87d85bbbc19
Anjalirajak205 <anjalirajak205@gmail.com>
2025-06-24 21:12:05 +0530

    24 june

--- SubstringSearch.java
+++ SubstringSearch.java
@@ -1,29 +1,31 @@
 // question 29/50
 // 15 june
 public class SubstringSearch{
-     public static String subString(String str, int a) {
-        char[] chars = str.toCharArray();
-        String result = "";
-        for (int i = a; i < chars.length; i++) {
-            result += chars[i];
+    public static int lengthOfLongestSub(String s) {
+        String current = "";
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            String ch = String.valueOf(s.charAt(i));
+
+            if (current.contains(ch)) {
+
+                current = current.substring(current.indexOf(ch) + 1);
+            }
+
+            current += ch;
+            maxLength = Math.max(maxLength, current.length());
         }
-        return result;
+
+        return maxLength;
     }
-    public static String subString(String str, int a, int b) {
-        char[] chars = str.toCharArray();
-        String result = "";
-        for (int i = a; i < b && i < chars.length; i++) {
-            result += chars[i];
-        }
-        return result;
-    }
+
     public static void main(String[] args) {
-        String s1 = "Hello World";
-        System.out.println(subString(s1, 5));      // " World"
-        System.out.println(subString(s1, 2, 7));   // "llo W"
-       // System.out.println(subString(s1, 2, 7));   // "llo
-        }
+        String input = "abcaabcbb";
+        int result = lengthOfLongestSub(input);
+        System.out.println("Longest substring length: " + result);
     }
+}
 
          
     
